88dd68f627f515073856598895157c6fae010860
Shuhei Iwamoto <unusmundus001@gmail.com>
2014-08-11 00:50:35 +0900

    add unity sample

---
+++ EditorSample/Assets/Script/Test/DelegateMain.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelegateMain : MonoBehaviour
+{
+		void Start ()
+		{
+				DelegateTest.GameStart (GameStart);
+				DelegateTest.GameStop (GameStop);
+				DelegateTest.GameFinish (GameFinish);
+		}
+
+		void GameStart ()
+		{
+				Debug.Log ("GameStart");
+		}
+
+		void GameStop ()
+		{
+				Debug.Log ("GameStop");
+		}
+
+		void GameFinish (string msg)
+		{
+				Debug.Log ("GameFinish" + msg);
+		}
+}
---
+++ EditorSample/Assets/Script/Test/DelegateTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelegateTest : MonoBehaviour
+{
+		public delegate void doSomething ();
+		public delegate void doString (string msg);
+
+		public static void GameStart (doSomething callback)
+		{
+				// ゲームスタート処理
+				Debug.Log ("GameStartの処理");
+				
+				// ゲームスタートの処理が終了したことを各クラスへ通知する
+				// たとえばキャラクターを初期化する等
+				callback ();
+		}
+
+		public static void GameStop (doSomething callback)
+		{
+				// ゲーム中断処理
+				Debug.Log ("GameStopの処理");
+				callback ();
+		}
+
+		public static void GameFinish (doString callback)
+		{
+				// ゲーム終了処理
+				Debug.Log ("GameFinishの処理");
+				callback ("処理の終了を送信する");
+		}
+
+
+}
