112da992097828389f54966bb94203f817ef5e00
Shuhei Iwamoto <unusmundus001@gmail.com>
2014-08-13 01:28:29 +0900

    Unity Editor sample

---
+++ EditorSample/Assets/WindowEditor/Editor/BaseEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class BaseEditor : EditorWindow {
+
+	// http://anchan828.hatenablog.jp/entry/2013/02/14/022551
+		
+	void Awake () {
+	}
+	// 通常のWindow
+	[MenuItem("CustomWindow/NormalWindow")]
+	static void OpenNormalWindow () {
+		GetWindow<NormalWindow> ();
+	}
+
+	// 必ず手前に表示し続けるWindow
+	[MenuItem("CustomWindow/ShowUtility")]
+	static void OpenShowUtility () {
+		NormalWindow window = CreateInstance<NormalWindow> ();
+		window.ShowUtility ();
+	}
+
+	[MenuItem("CustomWindow/ShowPopup")]
+	static void OpenShowPopUp () {
+		NormalWindow window = CreateInstance<NormalWindow> ();
+		window.ShowPopup ();
+	}
+
+	void Start () {
+
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ EditorSample/Assets/WindowEditor/Editor/NormalWindow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class NormalWindow : EditorWindow {
+	void Start () {
+	}
+	void Update () {
+	
+	}
+	void OnGUI () {
+		if (Event.current.keyCode == KeyCode.Escape && Event.current.type == EventType.KeyDown) {
+			Close ();
+		}
+	}
+}
